b86adb1f3d6ac922603221268cf81ef9036b101a
Inna Posukan <inna.posukansmi@gmail.com>
2023-11-28 14:45:25 +0200

    design of your ads page

--- Pet4YouClient/src/app/pet4you-v1/modules/announcement/pages/user-announcements-page/user-announcements-page.component.ts
+++ Pet4YouClient/src/app/pet4you-v1/modules/announcement/pages/user-announcements-page/user-announcements-page.component.ts
@@ -1,9 +1,37 @@
 import { Component } from '@angular/core';
+import { Subject } from 'rxjs';
+import { SelectItem } from 'primeng/api';
 
 @Component({
   selector: 'app-user-announcements-page',
   templateUrl: './user-announcements-page.component.html'
 })
 export class UserAnnouncementsPageComponent {
+    destroy = new Subject<any>();
+    types: SelectItem[] = [
+      { label: 'Chose type', value: null, disabled: true },
+      { label: 'Sell', value: null },
+      { label: 'Exchange', value: null },
+      { label: 'Buy', value: null },
+  ];
+  anotherOptions: SelectItem[] = [
+      { label: 'Chose type', value: null, disabled: true },
+      { label: 'Dog', value: null},
+      { label: 'Cat', value:null},
+      { label: 'Bird', value: null },
+      { label: 'Fish', value: null },
+      { label: 'Hamster', value: null },
+      { label: 'Rabbit', value: null },
+      { label: 'Reptile', value: null },
+      { label: 'Turtle', value: null },
+      { label: 'Other', value: null },
+  ];
+ 
 
+  selectedType: any;
+  selectedAnother: any;
+  
+  ngOnInit() {}
+
+  ngAfterViewInit() {}
 }
